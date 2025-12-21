bcd833daf60835f8cebe5488c0f2a7e1f7ff7800
Richard Liu <richardadesigner@gmail.com>
2025-12-20 22:50:01 -0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,8 @@ fetch("../iframeTemplate.html")
         html = html.replace("/*--|JavaScript|--*/", JSeditor.getValue());
         html = html.replace("--|AccessToken|--", IFRAMEACCESSTOKEN)
         document.getElementById("preview").srcdoc = html
-    })
+    });
+document.getElementById("consolepanels").style.display = "none";
 // - Iframe Token
 const GernerateTokenCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 let token = "";
@@ -85,17 +86,32 @@ document.getElementById("HTMLeditor").addEventListener("drop", function (e) {
         HTMLeditor.setValue(e.target.result)
     }
     reader.readAsText(files[0]);
-})
+});
 // - CSS
 const CSSeditor = ace.edit("CSSeditor");
 CSSeditor.setTheme("ace/theme/nord_dark");
 CSSeditor.session.setMode("ace/mode/css");
 CSSeditor.setReadOnly(false);
 CSSeditor.setFontSize(14);
-document.getElementById("CSSeditor").addEventListener("keydown", function (e) {
-    if (e.altKey && e.shiftKey && (e.key == "F")) {
+CSSeditor.commands.addCommand({
+    name: "format",
+    bindKey: { win: "Alt-Shift-F" },
+    exec: function () {
         CSSeditor.setValue(css_beautify(CSSeditor.getValue()))
     }
+});
+// -- Drop CSS file
+document.getElementById("CSSeditor").addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
+document.getElementById("CSSeditor").addEventListener("drop", function (e) {
+    e.preventDefault();
+    let files = e.dataTransfer.files;
+    let reader = new FileReader;
+    reader.onload = function (e) {
+        CSSeditor.setValue(e.target.result)
+    }
+    reader.readAsText(files[0]);
 });
 // - JS
 const JSeditor = ace.edit("JSeditor");
@@ -103,10 +119,25 @@ JSeditor.setTheme("ace/theme/nord_dark");
 JSeditor.session.setMode("ace/mode/javascript");
 JSeditor.setReadOnly(false);
 JSeditor.setFontSize(14);
-document.getElementById("JSeditor").addEventListener("keydown", function (e) {
-    if (e.altKey && e.shiftKey && (e.key == "F")) {
-        JSeditor.setValue(html_beautify(JSeditor.getValue()))
+JSeditor.commands.addCommand({
+    name: "format",
+    bindKey: { win: "Alt-Shift-F" },
+    exec: function () {
+        JSeditor.setValue(js_beautify(JSeditor.getValue()))
     }
+});
+// -- Drop CSS file
+document.getElementById("JSeditor").addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
+document.getElementById("JSeditor").addEventListener("drop", function (e) {
+    e.preventDefault();
+    let files = e.dataTransfer.files;
+    let reader = new FileReader;
+    reader.onload = function (e) {
+        JSeditor.setValue(e.target.result)
+    }
+    reader.readAsText(files[0]);
 });
 // Resize
 let isDrag = false;
@@ -179,8 +210,8 @@ window.addEventListener("message", function (e) {
             document.getElementById("console").appendChild(consoleLogMessage);
         } else if (e.data.type == "console.error") {
             let consoleLogMessage = document.createElement("div");
-            consoleLogMessage.innerHTML = "<span style=\"color:white;display:flex;align-items:center;justify-content:left;\"><img src=\"errorSymbol.png\" style=\"width:12px;height:12px;\">&nbsp;" + e.data.message + "</span>";
-            consoleLogMessage.style = "padding-left:2px;width:90%;margin-left:2px;margin-top:1px;margin-bottom:2px;background-color:pink;border-radius:2px;"
+            consoleLogMessage.innerHTML = "<span style=\"color:white;display:flex;align-items:center;justify-content:left;\"><img src=\"icons/errorSymbol.png\" style=\"width:12px;height:12px;pointer-events:none;\">&nbsp;" + e.data.message + "</span>";
+            consoleLogMessage.style = "padding-left:2px;width:90%;margin-left:2px;margin-top:1px;margin-bottom:2px;background-color:rgb(252, 167, 169);border-radius:2px;"
             document.getElementById("console").appendChild(consoleLogMessage);
         } else if (e.data.type == "console.clear") {
             document.getElementById("console").innerHTML = "";
@@ -197,5 +228,61 @@ document.getElementById("consoleInput").addEventListener("keydown", function (e)
             "verifyID": IFRAMEACCESSTOKEN,
             "content": document.getElementById("consoleInput").innerText
         }, "*");
+        document.getElementById("consoleInput").innerText = "";
     }
 });
+
+// Console Resize bar
+let isConsoleResize = false;
+let startY;
+let startBottom;
+document.getElementById("consoleResizeBar").addEventListener("mousedown", function (e) {
+    if (isConsoleopened) {
+        startY = e.y;
+        startBottom = parseFloat(window.getComputedStyle(document.getElementById("consoleResizeBar")).bottom);
+        isConsoleResize = true;
+    }
+});
+document.addEventListener("mouseup", function (e) {
+    isConsoleResize = false;
+})
+document.getElementById("previewArea").addEventListener("mouseout", function () {
+    isConsoleResize = false;
+})
+document.addEventListener("mousemove", function (e) {
+    if (isConsoleResize) {
+        let dy = e.y - startY;
+        let newPosition = startBottom - dy;
+        if (newPosition >= parseFloat(window.getComputedStyle(document.getElementById("previewArea")).height) - 26) {
+            newPosition = parseFloat(window.getComputedStyle(document.getElementById("previewArea")).height) - 26;
+        }
+        if (newPosition <= 80) {
+            newPosition = 80;
+        }
+        document.getElementById("consoleResizeBar").style.bottom = newPosition + "px";
+        document.getElementById("console").style.height = newPosition - 20 + "px";
+    }
+});
+// open and close console
+let isConsoleopened = false;
+document.getElementById("openOrCloseConsoleButton").addEventListener("click", function () {
+    document.getElementById("consoleResizeBar").style.transition = "0.5s";
+    document.getElementById("console").style.transition = "0.5s";
+    setTimeout(function () {
+        document.getElementById("consoleResizeBar").style.transition = "none";
+        document.getElementById("console").style.transition = "none";
+    }, 510)
+    if (isConsoleopened == false) {
+        isConsoleopened = true;
+        document.getElementById("consoleResizeBar").style.bottom = "120px";
+        document.getElementById("console").style.height = "100px";
+        document.getElementById("consolepanels").style.display = "revert";
+        document.getElementById("openOrCloseConsoleButton").innerText = "Close Console";
+    } else {
+        isConsoleopened = false;
+        document.getElementById("consoleResizeBar").style.bottom = "-1px";
+        document.getElementById("console").style.height = "0px";
+        document.getElementById("consolepanels").style.display = "none";
+        document.getElementById("openOrCloseConsoleButton").innerText = "Open Console";
+    }
+})
